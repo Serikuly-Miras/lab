66bed3c8d317d7a7109968141db399925f734774
Serikuly Miras <m.serikuly@alemagro.com>
2025-09-21 22:24:00 +0500

    Removed polars verbose output form 1brc dag

--- dags/1brc.py
+++ dags/1brc.py
@@ -18,32 +18,31 @@ def test_dag():
         source = f"s3://{bucket}/1brc/measurements.parquet"
 
         try:
-            with pl.Config(verbose=True):
-                print("Processing 1BRC data from S3 ...")
-                pl.Config.set_streaming_chunk_size(1_000_000)
-                result = (
-                    pl.scan_parquet(
-                        source,
-                        storage_options={
-                            "aws_endpoint_url": extras.get("endpoint_url"),
-                            "aws_access_key_id": conn.login,
-                            "aws_secret_access_key": conn.password,
-                        },
-                        n_rows=100_000_000,  # 1 / 10 of the data
-                        low_memory=True,
-                    )
-                    .group_by("station")
-                    .agg(
-                        [
-                            pl.col("temperature").min().alias("min"),
-                            pl.col("temperature").mean().round(2).alias("mean"),  # noqa
-                            pl.col("temperature").max().alias("max"),
-                        ]
-                    )
-                    .sort("station")
-                    .collect(engine="streaming")
+            print("Processing 1BRC data from S3 ...")
+            pl.Config.set_streaming_chunk_size(1_000_000)
+            result = (
+                pl.scan_parquet(
+                    source,
+                    storage_options={
+                        "aws_endpoint_url": extras.get("endpoint_url"),
+                        "aws_access_key_id": conn.login,
+                        "aws_secret_access_key": conn.password,
+                    },
+                    n_rows=100_000_000,  # 1 / 10 of the data
+                    low_memory=True,
                 )
-                print("... data processing complete.")
+                .group_by("station")
+                .agg(
+                    [
+                        pl.col("temperature").min().alias("min"),
+                        pl.col("temperature").mean().round(2).alias("mean"),  # noqa
+                        pl.col("temperature").max().alias("max"),
+                    ]
+                )
+                .sort("station")
+                .collect(engine="streaming")
+            )
+            print("... data processing complete.")
             print(result)
         except Exception as e:
             print(f"Error processing 1BRC data: {e}")
