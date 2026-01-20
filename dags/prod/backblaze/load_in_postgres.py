05417a1126f6fbdd79e116b36d90861fa1ad4b0e
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-01-20 14:54:02 +0500

    finishing loading data in load_backblaze_q3_to_postgres (still pretty dirty)

--- dags/prod/backblaze/load_in_postgres.py
+++ dags/prod/backblaze/load_in_postgres.py
@@ -1,5 +1,7 @@
-from airflow.sdk import Asset, dag, task
+import polars as pl
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.sdk import Asset, dag, task
+from airflow.sdk.bases.hook import BaseHook
 
 
 backblaze_q3_asset = Asset("s3://data-raw/backblaze/data_Q3_2025/")
@@ -24,8 +26,35 @@ def load_backblaze_q3_to_postgres():
         print(f"Found {len(s3_objects)} files in S3.")
         print("Files:", s3_objects)
 
+    # TODO clean and move work with creds into custom operators/hooks
+    @task(outlets=[postgres_dwh_asset])
+    def load_files_to_postgres(s3_objects):
+        s3_conn = BaseHook.get_connection("s3")
+        pg_conn = BaseHook.get_connection("dwh")
+
+        pg_uri = f"postgresql://{pg_conn.login}:{pg_conn.password}@{pg_conn.host}:{pg_conn.port}/{pg_conn.schema}"  # noqa
+
+        storage_options = {
+            "endpoint_url": s3_conn.extra_dejson.get("endpoint_url"),
+            "aws_access_key_id": s3_conn.login,
+            "aws_secret_access_key": s3_conn.password,
+        }
+
+        for i, obj in enumerate(s3_objects):
+            print(f"Processing {i + 1}/{len(s3_objects)}: {obj}")
+            pl.scan_csv(
+                f"s3://data-raw/{obj}",
+                storage_options=storage_options,
+            ).collect().write_database(
+                connection=pg_uri,
+                table_name="bronze.backblaze",
+                if_table_exists="replace" if i == 0 else "append",
+                engine="adbc",
+            )
+
     s3_objects = list_s3_files()
     print_s3_stats(s3_objects)
+    load_files_to_postgres(s3_objects)
 
 
 load_backblaze_q3_to_postgres()
