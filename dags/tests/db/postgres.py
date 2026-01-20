598053fa4c54b80f5885120162fea2d6a79f3d4f
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-01-20 11:19:59 +0500

    added SQLExecuteQueryOperator to postgres test dag

--- dags/tests/db/postgres.py
+++ dags/tests/db/postgres.py
@@ -1,4 +1,5 @@
 import polars as pl
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk import dag, task
 from airflow.sdk.bases.hook import BaseHook
 
@@ -9,13 +10,20 @@ from airflow.sdk.bases.hook import BaseHook
 )
 def test_dag():
     @task
-    def read_some() -> None:
+    def read_some_using_polars() -> None:
         c = BaseHook.get_connection("dwh")
         uri = f"postgresql://{c.login}:{c.password}@{c.host}:{c.port}/{c.schema}"  # noqa
         df = pl.read_database_uri(query="SELECT 1 as test;", uri=uri)
         print(df)
 
-    read_some()
+    read_some_using_operator = SQLExecuteQueryOperator(
+        task_id="read_some_using_operator",
+        conn_id="dwh",
+        sql="SELECT 1 as test;",
+    )
+
+    read_some_using_polars
+    read_some_using_operator
 
 
 test_dag()
