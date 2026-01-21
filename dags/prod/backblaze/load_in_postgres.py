052e230c5713a3250330f20910f6fcd84a1fa937
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-01-21 14:44:00 +0500

    reverted to using polars to load into poastgres (2x faster)

--- dags/prod/backblaze/load_in_postgres.py
+++ dags/prod/backblaze/load_in_postgres.py
@@ -1,11 +1,5 @@
-import os
-import tempfile
-import io
-import pandas as pd
-
-import boto3
+import polars as pl
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.sdk import Asset, dag, task
 from airflow.sdk.bases.hook import BaseHook
 
@@ -34,66 +28,46 @@ def load_backblaze_q3_to_postgres():
     @task(outlets=[postgres_dwh_asset])
     def load_files_to_postgres(s3_objects):
         s3_conn = BaseHook.get_connection("s3")
-        pg_hook = PostgresHook(postgres_conn_id="dwh")
+        pg_conn = BaseHook.get_connection("dwh")
 
-        s3_client = boto3.client(
-            "s3",
-            endpoint_url=s3_conn.extra_dejson.get("endpoint_url"),
-            aws_access_key_id=s3_conn.login,
-            aws_secret_access_key=s3_conn.password,
-        )
+        pg_uri = f"postgresql://{pg_conn.login}:{pg_conn.password}@{pg_conn.host}:{pg_conn.port}/{pg_conn.schema}"  # noqa
 
-        try:
-            pg_hook.run("DROP TABLE IF EXISTS bronze.backblaze;")
-            pg_hook.run(open(os.path.dirname(__file__) + "/create.sql").read())
-            pg_hook.run("ALTER TABLE bronze.backblaze SET UNLOGGED;")
+        storage_options = {
+            "endpoint_url": s3_conn.extra_dejson.get("endpoint_url"),
+            "aws_access_key_id": s3_conn.login,
+            "aws_secret_access_key": s3_conn.password,
+        }
 
-            for i, obj in enumerate(s3_objects):
-                print(f"Processing {i + 1}/{len(s3_objects)}: {obj}")
-                response = s3_client.get_object(Bucket="data-raw", Key=obj)
-                csv_content = response["Body"].read().decode("utf-8")
+        # read a little bit to infer and correct schema
+        df = pl.scan_csv(
+            f"s3://data-raw/{s3_objects[0]}",
+            storage_options=storage_options,
+            n_rows=1_000,
+        ).collect()
 
-                dtypes = {
-                    "serial_number": "string",
-                    "model": "string",
-                    "datacenter": "string",
-                    "capacity_bytes": "Int64",
-                    "failure": "Int64",
-                    "cluster_id": "Int64",
-                    "vault_id": "Int64",
-                    "pod_id": "Int64",
-                    "pod_slot_num": "Int64",
-                    "is_legacy_format": "boolean",
-                }
+        cols = [
+            pl.col(col).cast(pl.Int64)
+            for col in df.columns
+            if col.startswith("smart_")  # noqa
+        ]
+        cols.append(pl.col("date").cast(pl.Date))
 
-                # Add all SMART attributes as Int64
-                for attr_num in range(1, 256):
-                    dtypes[f"smart_{attr_num}_normalized"] = "Int64"
-                    dtypes[f"smart_{attr_num}_raw"] = "Int64"
-
-                df = pd.read_csv(
-                    io.StringIO(csv_content),
-                    dtype=dtypes,
+        for i, obj in enumerate(s3_objects):
+            print(f"Processing {i + 1}/{len(s3_objects)}: {obj}")
+            (
+                pl.scan_csv(
+                    f"s3://data-raw/{obj}",
+                    storage_options=storage_options,
                 )
-
-                with tempfile.NamedTemporaryFile(
-                    mode="w", delete=False, suffix=".csv"
-                ) as temp_file:
-                    temp_file.write(
-                        df.to_csv(
-                            sep="\t",
-                            index=False,
-                            header=False,
-                            na_rep="\\N",
-                        )
-                    )
-                    temp_file_path = temp_file.name
-                    try:
-                        pg_hook.bulk_load("bronze.backblaze", temp_file_path)
-                    finally:
-                        os.unlink(temp_file_path)
-        finally:
-            pg_hook.run("ALTER TABLE bronze.backblaze SET LOGGED;")
+                .with_columns(cols)
+                .collect()
+                .write_database(
+                    connection=pg_uri,
+                    table_name="bronze.backblaze",
+                    if_table_exists="replace" if i == 0 else "append",
+                    engine="adbc",
+                )
+            )
 
     s3_objects = list_s3_files()
     print_s3_stats(s3_objects)
