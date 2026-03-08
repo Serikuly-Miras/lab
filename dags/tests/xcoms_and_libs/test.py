fa2c33bd1a9e05305f6ab30fa59df297821e3c0c
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-03-08 19:32:09 +0500

    testing base64 over bytes of parquet to pass df in xcoms

--- dags/tests/xcoms_and_libs/test.py
+++ dags/tests/xcoms_and_libs/test.py
@@ -1,3 +1,4 @@
+import base64
 import io
 
 import pandas as pd
@@ -14,22 +15,22 @@ def test_dag():
     def pass_pandas_df_in_xcom() -> str:
         df = pd.DataFrame(data={"a": [i for i in range(50)]})
         print(df.describe())
-        return df.to_parquet()
+        return base64.b64encode(df.to_parquet()).decode("utf-8")
 
     @task
     def recieve_pandas_df_xcom(data: str):
-        data = pd.read_parquet(io.BytesIO(data))
+        data = pd.read_parquet(io.BytesIO(base64.b64decode(data)))
         print(data.describe())
 
     @task
     def pass_polars_df_in_xcom() -> str:
         df = pl.DataFrame(data={"a": [i for i in range(50)]})
         print(df.describe())
-        return df.write_parquet()
+        return base64.b64encode(df.write_parquet()).decode("utf-8")
 
     @task
     def recieve_polars_df_xcom(data: str):
-        data = pl.read_parquet(io.BytesIO(data))
+        data = pl.read_parquet(io.BytesIO(base64.b64decode(data)))
         print(data.describe())
 
     pd_df = pass_pandas_df_in_xcom()
