5dde5ca564b6c7ae5492f8904cb6ace37a1a4a23
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-04-01 15:45:42 +0500

    refactored load_backblaze_q3_to_ducklake

--- dags/prod/backblaze/load_in_ducklake.py
+++ dags/prod/backblaze/load_in_ducklake.py
@@ -1,3 +1,4 @@
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.sdk import Asset, dag, task
 from custom_operator.ducklake import DuckLakeHook
 
@@ -17,8 +18,8 @@ def load_backblaze_q3_to_ducklake():
         - Dataset: Drive Stats data
     """
 
-    @task(inlets=[backblaze_q3_asset], outlets=[ducklake_dwh_asset])
-    def load_files_to_ducklake():
+    @task(inlets=[backblaze_q3_asset])
+    def init_table():
         ducklake = DuckLakeHook()
         con = ducklake.get_connection()
 
@@ -41,14 +42,30 @@ def load_backblaze_q3_to_ducklake():
                 """  # noqa
             )
             print("Set partitioning on 'date' column for 'hard_drive_data'.")
+        finally:
+            con.close()
 
-            # Load data
-            con.execute(
-                """
-                    INSERT INTO hard_drive_data SELECT *
-                    FROM read_csv('s3://data-raw/backblaze/data_Q3_2025/*.csv');
-                """  # noqa
-            )
+    @task(outlets=[ducklake_dwh_asset])
+    def load_files_to_ducklake():
+        ducklake = DuckLakeHook()
+        con = ducklake.get_connection()
+        s3_hook = S3Hook(aws_conn_id="ducklake_s3")
+        raw_files = s3_hook.list_keys(
+            bucket_name="data-raw", prefix="backblaze/data_Q3_2025/"
+        )
+
+        try:
+            con.execute("SET preserve_insertion_order=false;")
+
+            for raw_file in sorted(raw_files):
+                if raw_file.endswith(".csv"):
+                    print(f"Inserting file {raw_file} ...")
+                    con.execute(
+                        """
+                        INSERT INTO hard_drive_data SELECT * FROM read_csv(?);
+                        """,
+                        [f"s3://data-raw/{raw_file}"],
+                    )
 
             # Verify data load
             result = con.execute(
@@ -61,7 +78,7 @@ def load_backblaze_q3_to_ducklake():
         finally:
             con.close()
 
-    load_files_to_ducklake()
+    init_table() >> load_files_to_ducklake()
 
 
 load_backblaze_q3_to_ducklake()
