a4da1635eac6bfd4d15f97fefa34e4e233ef4670
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-05-02 21:37:28 +0500

    adding mssql to streaming example

---
+++ infra/streaming/setup/mssql.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE DWH;
+
+use DWH;
+
+---
+
+SELECT @@SERVERNAME, @@VERSION;
+
+SELECT * 
+FROM sys.dm_server_services 
+WHERE servicename LIKE '%Agent%';
+
+---
+
+EXECUTE sys.sp_cdc_enable_db;
+
+CREATE TABLE DWH.dbo.NewTable (id int);
+
+EXECUTE sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'NewTable',
+    @role_name = N'cdc_Admin';
+
+---
+
+SELECT name, is_cdc_enabled 
+FROM sys.databases 
+WHERE name = DB_NAME();
+
+SELECT * 
+FROM cdc.change_tables;
+
+---
+
+INSERT INTO dbo.NewTable (id) VALUES (1), (2), (3);
+
+UPDATE dbo.NewTable SET id = 99 WHERE id = 1;
+
+DELETE FROM dbo.NewTable WHERE id = 2;
+
+---
+
+SELECT 
+    __$operation,   -- 1=delete, 2=insert, 3=before update, 4=after update
+    __$seqval,
+    id
+FROM cdc.dbo_NewTable_CT
+ORDER BY __$seqval;
