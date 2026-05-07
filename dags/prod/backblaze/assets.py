f79f7a33674364ad692ca81d09f00afe41c12d8f
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-05-07 21:34:17 +0500

    patched assets

--- dags/prod/backblaze/assets.py
+++ dags/prod/backblaze/assets.py
@@ -1,8 +1,8 @@
 from airflow.sdk import Asset
 
-backblaze_q1_2025_asset = Asset("s3://data-raw/backblaze/data_Q1_2025/")
-backblaze_q2_2025_asset = Asset("s3://data-raw/backblaze/data_Q2_2025/")
-backblaze_q3_2025_asset = Asset("s3://data-raw/backblaze/data_Q3_2025/")
-backblaze_q4_2025_asset = Asset("s3://data-raw/backblaze/data_Q4_2025/")
+backblaze_q1_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q1_2025/")
+backblaze_q2_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q2_2025/")
+backblaze_q3_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q3_2025/")
+backblaze_q4_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q4_2025/")
 
 backblaze_2025_starrocks_asset = Asset("starrocks://bronze/backblaze_drive_stats/")
