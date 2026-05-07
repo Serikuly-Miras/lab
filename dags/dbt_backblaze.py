aed6a2c941c121c244838ee2750d517974bb8fbc
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-05-07 18:29:25 +0500

    commented dbt dag for now

--- dags/dbt_backblaze.py
+++ dags/dbt_backblaze.py
@@ -1,24 +1,24 @@
-from cosmos import DbtDag, ProjectConfig, ProfileConfig
-from cosmos.profiles import PostgresUserPasswordProfileMapping
+# from cosmos import DbtDag, ProjectConfig, ProfileConfig
+# from cosmos.profiles import PostgresUserPasswordProfileMapping
 
-profile_config = ProfileConfig(
-    profile_name="backblaze",
-    target_name="dev",
-    profile_mapping=PostgresUserPasswordProfileMapping(
-        conn_id="dwh",
-        profile_args={"schema": "silver"},
-    ),
-)
+# profile_config = ProfileConfig(
+#     profile_name="backblaze",
+#     target_name="dev",
+#     profile_mapping=PostgresUserPasswordProfileMapping(
+#         conn_id="dwh",
+#         profile_args={"schema": "silver"},
+#     ),
+# )
 
-basic_cosmos_dag = DbtDag(
-    project_config=ProjectConfig(
-        "/opt/airflow/dags/repo/dags/dbt/backblaze",
-        install_dbt_deps=True,
-    ),
-    profile_config=profile_config,
-    operator_args={
-        "install_deps": True,
-        "full_refresh": True,
-    },
-    dag_id="backblaze_cosmos_silver_layer",
-)
+# basic_cosmos_dag = DbtDag(
+#     project_config=ProjectConfig(
+#         "/opt/airflow/dags/repo/dags/dbt/backblaze",
+#         install_dbt_deps=True,
+#     ),
+#     profile_config=profile_config,
+#     operator_args={
+#         "install_deps": True,
+#         "full_refresh": True,
+#     },
+#     dag_id="backblaze_cosmos_silver_layer",
+# )
