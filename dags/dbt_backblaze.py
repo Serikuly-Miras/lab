929e8a625559e259651388e08a918dc524bf88b8
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-05-08 00:40:42 +0500

    added a custom StarrocksNoPasswordProfileMapping

--- dags/dbt_backblaze.py
+++ dags/dbt_backblaze.py
@@ -1,10 +1,30 @@
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
-from cosmos.profiles import StarrocksUserPasswordProfileMapping
+from cosmos.profiles.starrocks.user_pass import StarrocksUserPasswordProfileMapping
+
+
+class StarrocksNoPasswordProfileMapping(StarrocksUserPasswordProfileMapping):
+    required_fields = [
+        "host",
+        "username",
+        "port",
+        "schema",
+    ]
+
+    secret_fields = []
+
+    @property
+    def profile(self):
+        profile = {
+            **self.mapped_params,
+            **self.profile_args,
+        }
+        return self.filter_null(profile)
+
 
 profile_config = ProfileConfig(
     profile_name="backblaze",
     target_name="dev",
-    profile_mapping=StarrocksUserPasswordProfileMapping(
+    profile_mapping=StarrocksNoPasswordProfileMapping(
         conn_id="starrocks",
         profile_args={"schema": "silver"},
     ),
