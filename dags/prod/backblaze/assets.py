cc3ceb748c60ad64ee4db628ab5b90b65861ae3b
Serikuly Miras <serikuly.miras.16@gmail.com>
2026-05-11 20:50:54 +0500

    dag s3_to_iceberg_backblaze_2025 wip

--- dags/prod/backblaze/assets.py
+++ dags/prod/backblaze/assets.py
@@ -6,3 +6,4 @@ backblaze_q3_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q3_2025/")
 backblaze_q4_2025_asset = Asset("s3://data-raw/backblaze-parquet/data_Q4_2025/")
 
 backblaze_2025_starrocks_asset = Asset("starrocks://bronze/backblaze_drive_stats/")
+backblaze_2025_iceberg_asset = Asset("iceberg://backblaze/backblaze_drive_stats/")
